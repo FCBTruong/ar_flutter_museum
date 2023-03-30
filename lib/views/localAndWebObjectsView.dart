a1c0694e99e4cc6f4a0895a0436db5da895b10f3
Truong <55469561+FCBTruong@users.noreply.github.com>
2023-03-30 22:55:57 +0700

    update

--- lib/views/localAndWebObjectsView.dart
+++ lib/views/localAndWebObjectsView.dart
@@ -7,6 +7,7 @@ import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
 import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
 import 'package:ar_flutter_plugin/datatypes/node_types.dart';
 import 'package:ar_flutter_plugin/models/ar_node.dart';
+import 'package:vector_math/vector_math_64.dart' hide Colors;
 
 class LocalAndWebObjectsView extends StatefulWidget {
   const LocalAndWebObjectsView({Key? key}) : super(key: key);
@@ -45,6 +46,40 @@ class _LocalAndWebObjectsViewState extends State<LocalAndWebObjectsView> {
     this.arObjectManager.onInitialize();
   }
 
+  Future<void> onLocalObjectButtonPressed() async {
+    // 1
+    if (localObjectNode != null) {
+      arObjectManager.removeNode(localObjectNode!);
+      localObjectNode = null;
+    } else {
+      // 2
+      var newNode = ARNode(
+          type: NodeType.localGLTF2,
+          uri: "assets/Chicken_01/Chicken_01.gltf",
+          scale: Vector3(0.2, 0.2, 0.2),
+          position: Vector3(0.0, 0.0, 0.0),
+          rotation: Vector4(1.0, 0.0, 0.0, 0.0));
+      // 3
+      bool? didAddLocalNode = await arObjectManager.addNode(newNode);
+      localObjectNode = (didAddLocalNode!) ? newNode : null;
+    }
+  }
+
+  Future<void> onWebObjectAtButtonPressed() async {
+    if (webObjectNode != null) {
+      arObjectManager.removeNode(webObjectNode!);
+      webObjectNode = null;
+    } else {
+      var newNode = ARNode(
+          type: NodeType.webGLB,
+          uri:
+              "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF-Binary/Duck.glb",
+          scale: Vector3(0.2, 0.2, 0.2));
+      bool? didAddWebNode = await arObjectManager.addNode(newNode);
+      webObjectNode = (didAddWebNode!) ? newNode : null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,7 +107,7 @@ class _LocalAndWebObjectsViewState extends State<LocalAndWebObjectsView> {
                 Expanded(
                   child: ElevatedButton(
                       onPressed: () {
-                        //TODO
+                        onLocalObjectButtonPressed();
                       },
                       child: const Text("Add / Remove Local Object")),
                 ),
@@ -82,7 +117,7 @@ class _LocalAndWebObjectsViewState extends State<LocalAndWebObjectsView> {
                 Expanded(
                   child: ElevatedButton(
                       onPressed: () {
-                        //TODO
+                        onWebObjectAtButtonPressed();
                       },
                       child: const Text("Add / Remove Web Object")),
                 )
