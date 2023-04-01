eed09f769ba9054806ebabd853effeabeb688a8f
Truong <55469561+FCBTruong@users.noreply.github.com>
2023-04-01 23:28:51 +0700

    update qr code

--- lib/views/homeView.dart
+++ lib/views/homeView.dart
@@ -1,5 +1,6 @@
 import 'package:arcore_example/views/localAndWebObjectsView.dart';
 import 'package:flutter/material.dart';
+import 'package:arcore_example/qr_code.dart';
 
 class MyHomePage extends StatelessWidget {
   final String title;
@@ -21,6 +22,38 @@ class MyHomePage extends StatelessWidget {
                         builder: (context) => const LocalAndWebObjectsView()));
               },
               child: const Text("Local / Web Objects")),
-        ));
+        ),
+        bottomNavigationBar: createBottomAppBar(context));
+  }
+
+  BottomAppBar createBottomAppBar(BuildContext context) {
+    return BottomAppBar(
+        color: Colors.white,
+        shape: CircularNotchedRectangle(),
+        clipBehavior: Clip.antiAlias,
+        notchMargin: 5.0,
+        child: Container(
+            height: 70,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // left tab bar icons
+                children: <Widget>[
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        // Study Icon
+                        MaterialButton(
+                          color: Colors.red,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50.0),
+                            ),
+                            minWidth: 50,
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => const QRViewExample(),
+            ));
+                            })
+                      ])
+                ])));
   }
 }
